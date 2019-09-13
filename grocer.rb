f5596f12ec19b715219e18f30995aa45b28e4336
Garrett Burns <garrettburns@mac.com>
2019-09-13 23:06:20 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,5 +1,25 @@
 def consolidate_cart(cart)
-  # code here
+  new_cart = []
+  #new_sans_count = new_cart.each {|item| item.reject {|k,v| k == [:count]}}
+  
+  cart.map {
+    |item|
+    p item
+    
+    
+    if new_cart.include?(item)
+      #item[:count] += 1
+      
+      p "true"
+    else
+      item[:count] = 1
+      new_cart.push(item)
+      p "false"
+    end
+    
+  }
+  
+  p new_cart
 end
 
 def apply_coupons(cart, coupons)
@@ -13,3 +33,10 @@ end
 def checkout(cart, coupons)
   # code here
 end
+
+consolidate_cart([
+    {"AVOCADO" => {:price => 3.00, :clearance => true}},
+    {"KALE" => {:price => 3.00, :clearance => false}},
+    {"AVOCADO" => {:price => 3.00, :clearance => true}},
+    {"KALE" => {:price => 3.00, :clearance => false}},
+  ])
