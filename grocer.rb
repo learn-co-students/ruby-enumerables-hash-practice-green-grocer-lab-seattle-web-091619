311261e72ba94d58d7e9a262971a71716ab55c82
Garrett Burns <garrettburns@mac.com>
2019-09-14 06:01:17 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,42 +1,57 @@
 def consolidate_cart(cart)
-  new_cart = []
-  #new_sans_count = new_cart.each {|item| item.reject {|k,v| k == [:count]}}
-  
-  cart.map {
-    |item|
-    p item
-    
-    
-    if new_cart.include?(item)
-      #item[:count] += 1
-      
-      p "true"
+  new_hash = {}
+  cart.each do |item|
+    if new_hash[item.keys[0]]
+      new_hash[item.keys[0]][:count] += 1
     else
-      item[:count] = 1
-      new_cart.push(item)
-      p "false"
+      new_hash[item.keys[0]] = {
+        count: 1,
+        price: item.values[0][:price],
+        clearance: item.values[0][:clearance]
+      }
     end
-    
-  }
-  
-  p new_cart
+  end
+  new_hash
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  coupons.each do |coupon|
+    if cart.keys.include? coupon[:item]
+      if cart[coupon[:item]][:count] >= coupon[:num]
+        new_name = "#{coupon[:item]} W/COUPON"
+        if cart[new_name]
+          cart[new_name][:count] += coupon[:num]
+        else
+          cart[new_name] = {
+            count: coupon[:num],
+            price: coupon[:cost]/coupon[:num],
+            clearance: cart[coupon[:item]][:clearance]
+          }
+        end
+        cart[coupon[:item]][:count] -= coupon[:num]
+      end
+    end
+  end
+  cart
 end
 
 def apply_clearance(cart)
-  # code here
+  cart.keys.each do |item|
+    if cart[item][:clearance]
+      cart[item][:price] = (cart[item][:price]*0.80).round(2)
+    end
+  end
+  cart
 end
 
 def checkout(cart, coupons)
-  # code here
-end
+  consol_cart = consolidate_cart(cart)
+  cart_with_coupons_applied = apply_coupons(consol_cart, coupons)
+  cart_with_discounts_applied = apply_clearance(cart_with_coupons_applied)
 
-consolidate_cart([
-    {"AVOCADO" => {:price => 3.00, :clearance => true}},
-    {"KALE" => {:price => 3.00, :clearance => false}},
-    {"AVOCADO" => {:price => 3.00, :clearance => true}},
-    {"KALE" => {:price => 3.00, :clearance => false}},
-  ])
+  total = 0.0
+  cart_with_discounts_applied.keys.each do |item|
+    total += cart_with_discounts_applied[item][:price]*cart_with_discounts_applied[item][:count]
+  end
+  total > 100.00 ? (total * 0.90).round : total
+end
